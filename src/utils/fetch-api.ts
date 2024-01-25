a24235a75a43c3b74bc1afc630e570cca113952c
Douglinhas <daguiaralcantara@gmail.com>
2024-01-25 20:19:21 -0300

    feat: create fetch functions

---
+++ src/utils/fetch-api.ts
@@ -0,0 +1,68 @@
+import { Contact } from "@/types/contacts";
+
+const token = "d83aa658-6caa-43ee-ab28-c76423dc6f05";
+const urlBase = `https://api.box3.work/api/Contato/${token}`;
+
+export const getContacts = async (): Promise<Contact[] | undefined> => {
+  try {
+    const response = await fetch(urlBase);
+    const lista = await response.json();
+    return lista;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getContactById = async (id: string): Promise<Contact | undefined> => {
+  try {
+    const response = await fetch(`${urlBase}/${id}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const createContact = async (contact: Contact): Promise<Contact | undefined> => {
+  try {
+    const response = await fetch(urlBase, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(contact),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const deleteContact = async (id: string): Promise<Contact | undefined> => {
+  try {
+    const response = await fetch(`${urlBase}/${id}`, {
+      method: "DELETE",
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const updateContact = async (id: string, contact: Contact): Promise<Contact | undefined> => {
+  try {
+    const response = await fetch(`${urlBase}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(contact),
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
